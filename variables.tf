c6eba647334f4fd222a192afbd7f8d05049cd7ef
yuvaraj <yuvarajhv1311@gmail.com>
2026-07-01 02:11:15 +0530

    terraform

--- variables.tf
+++ variables.tf
@@ -12,8 +12,8 @@ variable "component" {
 }
 
 variable "instance_type" {
-    default = t3.micro
     type = string
+    default = "t3.micro"
 
     validation {
         condition     = contains(["t3.micro", "t3.small", "t3.medium"], var.instance_type)
